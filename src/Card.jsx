456628ea2d41f53bfcdff06f5adc7d88834345fd
Adham-16 <a98347612z@gmail.com>
2025-04-09 07:02:01 +0200

    Update loading

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,10 +6,10 @@ export function Card({ card, index, updateRevealedCard, isRevealed }) {
   return (
     <div className={`card ${isRevealed ? 'is-revealed' : ''}`} onClick={handleClick}>
       <div className="card-face">
-        <img className="w-full h-3/4" alt="card-face" src={card.src} />
+        <img className="w-full h-3/4" loading="lazy" alt="card-face" src={card.src} />
       </div>
       <div className="card-back">
-        <img className="w-full h-3/4 " alt="card-back" src="/pix/16.avif" />
+        <img className="w-full h-3/4 " loading="lazy" alt="card-back" src="/pix/16.avif" />
       </div>
     </div>
   );
